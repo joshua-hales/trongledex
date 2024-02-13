0b035962c6f1974a336974df44ff2fa763747afc
Joshua Hales <joshuadanielhales@gmail.com>
2024-02-12 19:47:10 -0700

    move db to folder

--- src/trongledex_api/database.py
+++ src/trongledex_api/database.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, create_engine
 
-sqlite_file_name = "database.db"
+sqlite_file_name = "db/database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url)
